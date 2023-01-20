00fb331fbaddda8a8e9f4dd39bf61fa07cf95c90
MrQui <kathistbklemm@gmail.com>
2023-01-20 18:04:09 +0100

    updating

--- ant.py
+++ ant.py
@@ -1,7 +1,13 @@
 import home
 import random
+import pygame
 
 class Ant:
-    def __init__(self):
+    def __init__(self, screen: pygame.Surface):
         self.position = (random.randint(10, 590), random.randint(10, 590))
         self.size = (5, 10)
+        self.rotation = random.randint(0, 359)
+        self.speed = 1
+        self.color = (255, 0, 0)
+        self.ant = pygame.Rect(self.position[0], self.position[1], 5, 10)
+        self.ant = pygame.transform.rotate(screen, 90)
--- main.py
+++ main.py
@@ -5,19 +5,31 @@ import home
 import pheromone_home
 import pheromone_food
 
-# Creating pygame variables
-pygame.init()
-width = 600
-height = 600
-screen = pygame.display.set_mode([width, height], pygame.RESIZABLE)
 
 # Creating simulation variables
 ants = []
+FPS = 30
+width = 600
+height = 600
+
+
+# Creating pygame variables
+pygame.init()
+screen = pygame.display.set_mode([width, height], pygame.RESIZABLE)
+clock = pygame.time.Clock()
 
 # Creating Ants
-for i in range(50):
-    ants.append(ant.Ant())
+for i in range(1):
+    ants.append(ant.Ant(screen))
 
+
+rect = pygame.Rect(400, 400, 5, 10)
+# define a surface (RECTANGLE)
+image_orig = pygame.Surface((100 , 100))
+# for making transparent background while rotating an image
+image_orig.set_colorkey((0, 0, 0))
+# fill the rectangle / surface with green color
+image_orig.fill((0, 255, 0))
 
 running = True
 while running:
@@ -25,11 +37,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0, 0, 0))
+    clock.tick(FPS)
 
+    # making a copy of the old center of the rectangle
+    old_center = rect.center
+    # defining angle of the rotation
+    rot = (0 + 20) % 360
+    # rotating the orignal image
+    new_image = pygame.transform.rotate(image_orig, rot)
+    rect = new_image.get_rect()
+    # set the rotated rectangle to the old center
+    rect.center = old_center
+    # drawing the rotated rectangle to the screen
+    screen.blit(new_image, rect)
 
     # Drawing Ants
-    for i in ants:
-        pygame.draw.rect(screen, (255, 0, 0), (i.position[0], i.position[1], i.size[0], i.size[1]))
+    for ant in ants:
+        pygame.draw.rect(screen, ant.color, (ant.position[0], ant.position[1], ant.size[0], ant.size[1]))
     # Drawing Home
     pygame.draw.rect(screen, (139, 69, 19), (home.pos[0], home.pos[1], home.size[0], home.size[1]))
 
