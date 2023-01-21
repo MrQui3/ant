390d4e82bbebf1a4d00375706b0bf195e59b2ff8
MrQui <kathistbklemm@gmail.com>
2023-01-21 15:02:51 +0100

    updating variables

--- ant.py
+++ ant.py
@@ -5,9 +5,6 @@ import home
 
 class Ant:
     def __init__(self):
-        self.rotation = random.randint(0, 359)
-        self.speed = 1
-        self.color = (255, 0, 0)
         self.ant = pygame.Rect(home.pos[0], home.pos[1], 10, 10)
         self.target = (random.randint(-200, 200), random.randint(-200, 200))
 
