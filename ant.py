f56053f8a4aaa9f45b0f588d0a5fc2cac3fac50c
MrQui <kathistbklemm@gmail.com>
2023-01-22 12:08:21 +0100

    updating variables

--- ant.py
+++ ant.py
@@ -7,6 +7,8 @@ class Ant:
     def __init__(self):
         self.ant = pygame.Rect(home.pos[0], home.pos[1], 10, 10)
         self.target = (random.randint(-200, 200), random.randint(-200, 200))
+        self.having_target = False
+        self.pheromone_time = 0
 
     def new_target(self):
         self.target = (random.randint(-200, 200), random.randint(-200, 200))
